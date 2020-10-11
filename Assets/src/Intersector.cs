9814093bc18764c0a7af0ca7077d458f3faa4e8a
Bezhok Nikita <30340548+Bezhok@users.noreply.github.com>
2020-10-11 16:11:31 +0300

    First version

---
+++ Assets/src/Intersector.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections.Generic;
+using Plugins;
+using UnityEngine;
+
+namespace src
+{
+    public class Intersector
+    {
+        private readonly Mesh _mesh;
+        private readonly List<Vector3> _normals;
+        private readonly Vector3 _planeNormal;
+        private readonly Vector3 _planePoint;
+        private readonly GameObject _srcObject;
+        private readonly List<Vector4> _tangents;
+        private readonly List<int> _triangles;
+        private readonly List<Vector2> _uvs;
+        private readonly List<Vector3> _verts;
+
+        public Mesh CreateMesh()
+        {
+            var newMesh = new Mesh();
+            newMesh.vertices = _verts.ToArray();
+            newMesh.normals = _normals.ToArray();
+            newMesh.uv = _uvs.ToArray();
+            newMesh.triangles = _triangles.ToArray();
+            newMesh.tangents = _tangents.ToArray();
+
+            return newMesh;
+        }
+        public Intersector(Vector3 planePoint,
+            Vector3 planeNormal, GameObject srcObject, Mesh mesh)
+        {
+            _planeNormal = planeNormal;
+            _planePoint = planePoint;
+            _srcObject = srcObject;
+            _mesh = mesh;
+
+            _verts = new List<Vector3>();
+
+            _normals = new List<Vector3>();
+
+            _uvs = new List<Vector2>();
+
+            _triangles = new List<int>();
+
+            _tangents = new List<Vector4>();
+        }
+
+        private Vector3 IntersectionPlaneLinePoint(Vector3 pointF, Vector3 pointS, Vector3 planePoint,
+            Vector3 planeNormal)
+        {
+            var t = Vector3.Dot(planeNormal, planePoint - pointF) /
+                    Vector3.Dot(planeNormal, pointS - pointF);
+            var intersectionPoint = pointF + t * (pointS - pointF);
+            return intersectionPoint;
+        }
+
+        //                   leftPoint
+        //                   |\
+        //                   | \
+        // intersectionPoint2|__\ intersectionPoint1
+        //                   |   \
+        //        rightPoint2|____\ rightPoint1
+        //
+        public void CreateTriangleOnePointOnLeft(Vector3 leftPoint, Vector3 rightPoint1, Vector3 rightPoint2, int i,
+            int n0, int n1, int n2)
+        {
+            var n = _verts.Count;
+            var intersectionPoint1 = IntersectionPlaneLinePoint(leftPoint, rightPoint1, _planePoint, _planeNormal);
+            var intersectionPoint2 = IntersectionPlaneLinePoint(leftPoint, rightPoint2, _planePoint, _planeNormal);
+
+            var localLeftPoint1 = _srcObject.transform.InverseTransformPoint(leftPoint);
+            var localIntersectionPoint1 = _srcObject.transform.InverseTransformPoint(intersectionPoint1);
+            var localIntersectionPoint2 = _srcObject.transform.InverseTransformPoint(intersectionPoint2);
+
+            var localRightPoint1 = _srcObject.transform.InverseTransformPoint(rightPoint1);
+            var localRightPoint2 = _srcObject.transform.InverseTransformPoint(rightPoint2);
+
+            AddTangents(_mesh.tangents[_mesh.triangles[i + n0]], _mesh.tangents[_mesh.triangles[i + n1]],
+                _mesh.tangents[_mesh.triangles[i + n2]]);
+
+            var b1 = new Barycentric(localLeftPoint1, localRightPoint1, localRightPoint2, localIntersectionPoint1);
+            var localIntersectionPoint1Uv = b1.Interpolate(_mesh.uv[_mesh.triangles[i + n0]],
+                _mesh.uv[_mesh.triangles[i + n1]],
+                _mesh.uv[_mesh.triangles[i + n2]]);
+
+            var b2 = new Barycentric(localLeftPoint1, localRightPoint1, localRightPoint2, localIntersectionPoint2);
+            var localIntersectionPoint2Uv = b2.Interpolate(_mesh.uv[_mesh.triangles[i + n0]],
+                _mesh.uv[_mesh.triangles[i + n1]],
+                _mesh.uv[_mesh.triangles[i + n2]]);
+
+            AddUVs(_mesh.uv[_mesh.triangles[i + n0]], localIntersectionPoint1Uv, localIntersectionPoint2Uv);
+
+            var b3 = new Barycentric(localLeftPoint1, localRightPoint1, localRightPoint2, localIntersectionPoint1);
+            var localIntersectionPoint1Normal = b3.Interpolate(_mesh.normals[_mesh.triangles[i + n0]],
+                _mesh.normals[_mesh.triangles[i + n1]],
+                _mesh.normals[_mesh.triangles[i + n2]]);
+
+            var b4 = new Barycentric(localLeftPoint1, localRightPoint1, localRightPoint2, localIntersectionPoint2);
+            var localIntersectionPoint2Normal = b4.Interpolate(_mesh.normals[_mesh.triangles[i + n0]],
+                _mesh.normals[_mesh.triangles[i + n1]],
+                _mesh.normals[_mesh.triangles[i + n2]]);
+
+            AddNormals(_mesh.normals[_mesh.triangles[i + n0]], localIntersectionPoint1Normal,
+                localIntersectionPoint2Normal);
+            AddVerts(localLeftPoint1, localIntersectionPoint1, localIntersectionPoint2);
+
+
+            if (n0 == 0 || n0 == 2)
+                AddTriangle(n, 0, 1, 2);
+            else //if (n0 == 1)
+                AddTriangle(n, 0, 2, 1);
+        }
+
+        //                   rightPoint1
+        //                   |\
+        //                   | \
+        // intersectionPoint2|__\ intersectionPoint1
+        //                   |   \
+        //        leftPoint2 |____\ leftPoint1
+        //
+        public void CreateTrianglesTwoPointsOnLeft(Vector3 leftPoint1, Vector3 leftPoint2, Vector3 rightPoint1, int i,
+            int n0, int n1, int n2)
+        {
+            var n = _verts.Count;
+            var intersectionPoint1 = IntersectionPlaneLinePoint(leftPoint1, rightPoint1, _planePoint, _planeNormal);
+            var intersectionPoint2 = IntersectionPlaneLinePoint(leftPoint2, rightPoint1, _planePoint, _planeNormal);
+
+            var localLeftPoint1 = _srcObject.transform.InverseTransformPoint(leftPoint1);
+            var localLeftPoint2 = _srcObject.transform.InverseTransformPoint(leftPoint2);
+
+            var localIntersectionPoint1 = _srcObject.transform.InverseTransformPoint(intersectionPoint1);
+            var localIntersectionPoint2 = _srcObject.transform.InverseTransformPoint(intersectionPoint2);
+
+            var localRightPoint1 = _srcObject.transform.InverseTransformPoint(rightPoint1);
+
+            AddTangents(_mesh.tangents[_mesh.triangles[i + n0]],
+                _mesh.tangents[_mesh.triangles[i + n1]],
+                _mesh.tangents[_mesh.triangles[i + n2]],
+                _mesh.tangents[_mesh.triangles[i + n2]]);
+
+
+            var b1 = new Barycentric(localLeftPoint1, localLeftPoint2, localRightPoint1, localIntersectionPoint1);
+            var intersectionPoint1Uv = b1.Interpolate(_mesh.uv[_mesh.triangles[i + n0]],
+                _mesh.uv[_mesh.triangles[i + n1]],
+                _mesh.uv[_mesh.triangles[i + n2]]);
+
+            var b2 = new Barycentric(localLeftPoint1, localLeftPoint2, localRightPoint1, localIntersectionPoint2);
+            var intersectionPoint2Uv = b2.Interpolate(_mesh.uv[_mesh.triangles[i + n0]],
+                _mesh.uv[_mesh.triangles[i + n1]],
+                _mesh.uv[_mesh.triangles[i + n2]]);
+
+            AddUVs(_mesh.uv[_mesh.triangles[i + n0]],
+                _mesh.uv[_mesh.triangles[i + n1]],
+                intersectionPoint1Uv,
+                intersectionPoint2Uv);
+
+
+            var b3 = new Barycentric(localLeftPoint1, localLeftPoint2, localRightPoint1, localIntersectionPoint1);
+            var localIntersectionPoint1Normal = b3.Interpolate(_mesh.normals[_mesh.triangles[i + n0]],
+                _mesh.normals[_mesh.triangles[i + n1]],
+                _mesh.normals[_mesh.triangles[i + n2]]);
+
+            var b4 = new Barycentric(localLeftPoint1, localLeftPoint2, localRightPoint1, localIntersectionPoint2);
+            var localIntersectionPoint2Normal = b4.Interpolate(_mesh.normals[_mesh.triangles[i + n0]],
+                _mesh.normals[_mesh.triangles[i + n1]],
+                _mesh.normals[_mesh.triangles[i + n2]]);
+
+            AddNormals(_mesh.normals[_mesh.triangles[i + n0]],
+                _mesh.normals[_mesh.triangles[i + n1]],
+                localIntersectionPoint1Normal,
+                localIntersectionPoint2Normal);
+
+            AddVerts(localLeftPoint1,
+                localLeftPoint2,
+                localIntersectionPoint1,
+                localIntersectionPoint2);
+
+
+            if (n0 == 0 && n1 == 2)
+            {
+                AddTriangle(n, 1, 0, 2);
+                AddTriangle(n, 1, 2, 3);
+            }
+            else //if (n0 == 1)
+            {
+                AddTriangle(n, 0, 1, 3);
+                AddTriangle(n, 0, 3, 2);
+            }
+        }
+
+        private void AddTangents(Vector4 n1, Vector4 n2, Vector4 n3, Vector4 n4)
+        {
+            _tangents.Add(n1);
+            _tangents.Add(n2);
+            _tangents.Add(n3);
+            _tangents.Add(n4);
+        }
+
+        private void AddUVs(Vector2 n1, Vector2 n2, Vector2 n3, Vector2 n4)
+        {
+            _uvs.Add(n1);
+            _uvs.Add(n2);
+            _uvs.Add(n3);
+            _uvs.Add(n4);
+        }
+
+        private void AddNormals(Vector3 n1, Vector3 n2, Vector3 n3, Vector3 n4)
+        {
+            _normals.Add(n1);
+            _normals.Add(n2);
+            _normals.Add(n3);
+            _normals.Add(n4);
+        }
+
+        private void AddVerts(Vector3 n1, Vector3 n2, Vector3 n3, Vector3 n4)
+        {
+            _verts.Add(n1);
+            _verts.Add(n2);
+            _verts.Add(n3);
+            _verts.Add(n4);
+        }
+
+        private void AddTriangle(int n, int i0, int i1, int i2)
+        {
+            _triangles.Add(n + i0);
+            _triangles.Add(n + i1);
+            _triangles.Add(n + i2);
+        }
+
+        private void AddTangents(Vector4 n1, Vector4 n2, Vector4 n3)
+        {
+            _tangents.Add(n1);
+            _tangents.Add(n2);
+            _tangents.Add(n3);
+        }
+
+        private void AddUVs(Vector2 n1, Vector2 n2, Vector2 n3)
+        {
+            _uvs.Add(n1);
+            _uvs.Add(n2);
+            _uvs.Add(n3);
+        }
+
+        private void AddNormals(Vector3 n1, Vector3 n2, Vector3 n3)
+        {
+            _normals.Add(n1);
+            _normals.Add(n2);
+            _normals.Add(n3);
+        }
+
+        private void AddVerts(Vector3 n1, Vector3 n2, Vector3 n3)
+        {
+            _verts.Add(n1);
+            _verts.Add(n2);
+            _verts.Add(n3);
+        }
+    }
+}
---
+++ Assets/src/Slicer.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Plugins;
+using UnityEngine;
+
+namespace src
+{
+    public class Slicer : MonoBehaviour
+    {
+        private Mesh _lowerMesh;
+        private Mesh _upperMesh;
+        private GameObject _lowerObj;
+        private GameObject _upperObj;
+
+        private Mesh _mesh;
+
+        private Vector3 _prevSlicerPos;
+        private Vector3 _prevSlicerRotation;
+        private Mesh _slicerMesh;
+
+        private Vector3 _slicerNormal;
+        private Vector3 _slicerPoint;
+        
+
+        [SerializeField] private GameObject slicerQuad;
+        [SerializeField] private GameObject srcObject;
+
+
+        private bool IsPointLower(Vector3 point, Vector3 planePoint, Vector3 planeNormal)
+        {
+            return Vector3.Dot(planePoint - point, planeNormal) < 0;
+        }
+
+        public bool shouldDisplayLowerSide = true;
+        public bool shouldDisplayUpperSide = false;
+        private void Slice()
+        {
+            var srcVerts = _mesh.vertices;
+            var srcEbo = _mesh.triangles;
+            
+            var lowerEbo = new List<int>();
+            var upperEbo = new List<int>();
+            var upperMesh = Instantiate(_mesh);
+            var lowerMesh = Instantiate(_mesh);
+            
+            _slicerNormal = slicerQuad.transform.TransformDirection(_slicerMesh.normals[0]);
+            _slicerPoint = slicerQuad.transform.TransformPoint(_slicerMesh.vertices[0]);
+
+            var interLow = new Intersector(_slicerPoint, _slicerNormal, srcObject, _mesh);
+            var interUp = new Intersector(_slicerPoint, _slicerNormal, srcObject, _mesh);
+            for (var i = 0; i < _mesh.triangles.Length; i += 3)
+            {
+                var objVert1 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i]]);
+                var objVert2 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 1]]);
+                var objVert3 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 2]]);
+
+                var isFirstLower = IsPointLower(objVert1, _slicerPoint, _slicerNormal);
+                var isSecondLower = IsPointLower(objVert2, _slicerPoint, _slicerNormal);
+                var isThirdLower = IsPointLower(objVert3, _slicerPoint, _slicerNormal);
+
+                if (isFirstLower && isSecondLower && isThirdLower)
+                {
+                    if (shouldDisplayLowerSide)
+                    {
+                        lowerEbo.Add(srcEbo[i]);
+                        lowerEbo.Add(srcEbo[i + 1]);
+                        lowerEbo.Add(srcEbo[i + 2]);
+                    }
+                } 
+                else if (!isFirstLower && !isSecondLower && !isThirdLower)
+                {
+                    if (shouldDisplayUpperSide)
+                    {
+                        upperEbo.Add(srcEbo[i]);
+                        upperEbo.Add(srcEbo[i + 1]);
+                        upperEbo.Add(srcEbo[i + 2]);
+                    }
+                }
+                else
+                {
+                    if (shouldDisplayLowerSide)
+                        CreateTriangle(interLow, i, objVert1, objVert2, objVert3, isFirstLower, isSecondLower, isThirdLower);
+                    
+                    if (shouldDisplayUpperSide)
+                        CreateTriangle(interUp, i, objVert1, objVert2, objVert3, !isFirstLower, !isSecondLower, !isThirdLower);
+                }
+            }
+
+            if (shouldDisplayLowerSide)
+            {
+                _lowerObj = Instantiate(srcObject);
+                _lowerObj.GetComponent<MeshFilter>().sharedMesh = interLow.CreateMesh();
+                lowerMesh.triangles = lowerEbo.ToArray();
+                srcObject.GetComponent<MeshFilter>().sharedMesh = lowerMesh;
+            }
+
+            if (shouldDisplayUpperSide)
+            {
+                _upperObj = Instantiate(srcObject);
+                _upperObj.GetComponent<MeshFilter>().sharedMesh = interUp.CreateMesh();
+
+                upperMesh.triangles = upperEbo.ToArray();
+                srcObject.GetComponent<MeshFilter>().sharedMesh = upperMesh;
+            }
+        }
+
+        private void CreateTriangle(Intersector inter, int i, Vector3 objVert1, Vector3 objVert2, Vector3 objVert3,  bool isFirstLower, bool isSecondLower, bool isThirdLower)
+        {
+            if (isFirstLower && !isSecondLower && !isThirdLower)
+            {
+                inter.CreateTriangleOnePointOnLeft(objVert1, objVert2, objVert3, i, 0, 1, 2);
+            }
+            else if (isSecondLower && !isFirstLower && !isThirdLower)
+            {
+                inter.CreateTriangleOnePointOnLeft(objVert2, objVert1, objVert3, i, 1, 0, 2);
+            }
+            else if (isThirdLower && !isFirstLower && !isSecondLower)
+            {
+                inter.CreateTriangleOnePointOnLeft(objVert3, objVert1, objVert2, i, 2, 0, 1);
+            }
+            else if (isFirstLower && isSecondLower && !isThirdLower)
+            {
+                inter.CreateTrianglesTwoPointsOnLeft(objVert1, objVert2, objVert3, i, 0, 1, 2);
+            }
+            else if (!isFirstLower && isSecondLower && isThirdLower)
+            {
+                inter.CreateTrianglesTwoPointsOnLeft(objVert2, objVert3, objVert1, i, 1, 2, 0);
+            }
+            else if (isFirstLower && !isSecondLower && isThirdLower)
+            {
+                inter.CreateTrianglesTwoPointsOnLeft(objVert1, objVert3, objVert2, i, 0, 2, 1);
+            }
+        }
+        private void Start()
+        {
+            _mesh = srcObject.GetComponent<MeshFilter>().sharedMesh;
+            _slicerMesh = slicerQuad.GetComponent<MeshFilter>().sharedMesh;
+            Slice();
+            _prevSlicerPos = slicerQuad.transform.position;
+            _prevSlicerRotation = slicerQuad.transform.rotation.eulerAngles;
+        }
+
+        private void Update()
+        {
+            if ((slicerQuad.transform.position - _prevSlicerPos).magnitude > 0.001f ||
+                (slicerQuad.transform.rotation.eulerAngles - _prevSlicerRotation).magnitude > 0.001f)
+            {
+                Destroy(_lowerObj);
+                Destroy(_upperObj);
+                Slice();
+            }
+
+            _prevSlicerPos = slicerQuad.transform.position;
+            _prevSlicerRotation = slicerQuad.transform.eulerAngles;
+        }
+    }
+}
