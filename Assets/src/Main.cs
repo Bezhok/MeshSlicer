9ee5e16585558e8fd5ce218151b3675154756188
Bezhok Nikita <30340548+Bezhok@users.noreply.github.com>
2020-10-14 21:53:47 +0300

    added slicing for objects with childs && refactoring

--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -1,18 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
+namespace src
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Main : MonoBehaviour
     {
-        
-    }
+        private Mesh _mesh;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Vector3 _prevSlicerPos;
+        private Vector3 _prevSlicerRotation;
+        private Mesh _slicerMesh;
+
+        private Vector3 _slicerNormal;
+        private Vector3 _slicerPoint;
+    
+        private List<Slicer> _slicers;
+
+        public bool shouldDisplayLowerSide = true;
+        public bool shouldDisplayUpperSide = false;
+
+        [SerializeField] private GameObject slicerQuad;
+        [SerializeField] private GameObject srcObject;
+
+        private void Start()
+        {
+            Component[] renderers = srcObject.GetComponentsInChildren(typeof(MeshFilter), true);
+
+            _slicerMesh = slicerQuad.GetComponent<MeshFilter>().sharedMesh;
+            _slicerNormal = slicerQuad.transform.TransformDirection(_slicerMesh.normals[0]);
+            _slicerPoint = slicerQuad.transform.TransformPoint(_slicerMesh.vertices[0]);
+            _slicers = new List<Slicer>();
+
+            foreach (var component in renderers)
+            {
+                var meshRenderer = (MeshFilter) component;
+                if (meshRenderer.mesh != null && meshRenderer.mesh.triangles != null &&
+                    meshRenderer.mesh.triangles.Length != 0)
+                {
+                    var slicer = new Slicer(meshRenderer.sharedMesh, meshRenderer.gameObject);
+                    _slicers.Add(slicer);
+                    slicer.Slice(_slicerNormal, _slicerPoint, shouldDisplayLowerSide, shouldDisplayUpperSide);
+                }
+            }
+
+            _prevSlicerPos = slicerQuad.transform.position;
+            _prevSlicerRotation = slicerQuad.transform.rotation.eulerAngles;
+        }
+
+        private void Update()
+        {
+            if ((slicerQuad.transform.position - _prevSlicerPos).magnitude > 0.001f ||
+                (slicerQuad.transform.rotation.eulerAngles - _prevSlicerRotation).magnitude > 0.001f)
+            {
+                _slicerNormal = slicerQuad.transform.TransformDirection(_slicerMesh.normals[0]);
+                _slicerPoint = slicerQuad.transform.TransformPoint(_slicerMesh.vertices[0]);
+
+                foreach (var slicer in _slicers)
+                {
+                    slicer.Destroy();
+                    slicer.Slice(_slicerNormal, _slicerPoint, shouldDisplayLowerSide, shouldDisplayUpperSide);
+                }
+            }
+
+            _prevSlicerPos = slicerQuad.transform.position;
+            _prevSlicerRotation = slicerQuad.transform.eulerAngles;
+        }
     }
 }
--- Assets/src/Slicer.cs
+++ Assets/src/Slicer.cs
@@ -1,62 +1,54 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Plugins;
 using UnityEngine;
 
 namespace src
 {
-    public class Slicer : MonoBehaviour
+    public class Slicer
     {
-        private Mesh _lowerMesh;
-        private Mesh _upperMesh;
+        private readonly Mesh _mesh;
+        private readonly GameObject _srcObject;
         private GameObject _lowerObj;
         private GameObject _upperObj;
 
-        private Mesh _mesh;
+        public Slicer(Mesh mesh, GameObject gameObject)
+        {
+            _mesh = mesh;
+            _srcObject = gameObject;
+        }
 
-        private Vector3 _prevSlicerPos;
-        private Vector3 _prevSlicerRotation;
-        private Mesh _slicerMesh;
-
-        private Vector3 _slicerNormal;
-        private Vector3 _slicerPoint;
-        
-
-        [SerializeField] private GameObject slicerQuad;
-        [SerializeField] private GameObject srcObject;
-
+        public void Destroy()
+        {
+            Object.Destroy(_lowerObj);
+            Object.Destroy(_upperObj);
+        }
 
         private bool IsPointLower(Vector3 point, Vector3 planePoint, Vector3 planeNormal)
         {
             return Vector3.Dot(planePoint - point, planeNormal) < 0;
         }
 
-        public bool shouldDisplayLowerSide = true;
-        public bool shouldDisplayUpperSide = false;
-        private void Slice()
+        public void Slice(Vector3 slicerNormal, Vector3 slicerPoint, bool shouldDisplayLowerSide,
+            bool shouldDisplayUpperSide)
         {
             var srcVerts = _mesh.vertices;
             var srcEbo = _mesh.triangles;
-            
+
             var lowerEbo = new List<int>();
             var upperEbo = new List<int>();
-            var upperMesh = Instantiate(_mesh);
-            var lowerMesh = Instantiate(_mesh);
-            
-            _slicerNormal = slicerQuad.transform.TransformDirection(_slicerMesh.normals[0]);
-            _slicerPoint = slicerQuad.transform.TransformPoint(_slicerMesh.vertices[0]);
+            var upperMesh = Object.Instantiate(_mesh);
+            var lowerMesh = Object.Instantiate(_mesh);
 
-            var interLow = new Intersector(_slicerPoint, _slicerNormal, srcObject, _mesh);
-            var interUp = new Intersector(_slicerPoint, _slicerNormal, srcObject, _mesh);
+            var interLow = new Intersector(slicerPoint, slicerNormal, _srcObject, _mesh);
+            var interUp = new Intersector(slicerPoint, slicerNormal, _srcObject, _mesh);
             for (var i = 0; i < _mesh.triangles.Length; i += 3)
             {
-                var objVert1 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i]]);
-                var objVert2 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 1]]);
-                var objVert3 = srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 2]]);
+                var objVert1 = _srcObject.transform.TransformPoint(srcVerts[srcEbo[i]]);
+                var objVert2 = _srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 1]]);
+                var objVert3 = _srcObject.transform.TransformPoint(srcVerts[srcEbo[i + 2]]);
 
-                var isFirstLower = IsPointLower(objVert1, _slicerPoint, _slicerNormal);
-                var isSecondLower = IsPointLower(objVert2, _slicerPoint, _slicerNormal);
-                var isThirdLower = IsPointLower(objVert3, _slicerPoint, _slicerNormal);
+                var isFirstLower = IsPointLower(objVert1, slicerPoint, slicerNormal);
+                var isSecondLower = IsPointLower(objVert2, slicerPoint, slicerNormal);
+                var isThirdLower = IsPointLower(objVert3, slicerPoint, slicerNormal);
 
                 if (isFirstLower && isSecondLower && isThirdLower)
                 {
@@ -66,7 +58,7 @@ namespace src
                         lowerEbo.Add(srcEbo[i + 1]);
                         lowerEbo.Add(srcEbo[i + 2]);
                     }
-                } 
+                }
                 else if (!isFirstLower && !isSecondLower && !isThirdLower)
                 {
                     if (shouldDisplayUpperSide)
@@ -79,79 +71,52 @@ namespace src
                 else
                 {
                     if (shouldDisplayLowerSide)
-                        CreateTriangle(interLow, i, objVert1, objVert2, objVert3, isFirstLower, isSecondLower, isThirdLower);
-                    
+                        CreateTriangle(interLow, i, objVert1, objVert2, objVert3, isFirstLower, isSecondLower,
+                            isThirdLower);
+
                     if (shouldDisplayUpperSide)
-                        CreateTriangle(interUp, i, objVert1, objVert2, objVert3, !isFirstLower, !isSecondLower, !isThirdLower);
+                        CreateTriangle(interUp, i, objVert1, objVert2, objVert3, !isFirstLower, !isSecondLower,
+                            !isThirdLower);
                 }
             }
 
-            if (shouldDisplayLowerSide)
-            {
-                _lowerObj = Instantiate(srcObject);
-                _lowerObj.GetComponent<MeshFilter>().sharedMesh = interLow.CreateMesh();
-                lowerMesh.triangles = lowerEbo.ToArray();
-                srcObject.GetComponent<MeshFilter>().sharedMesh = lowerMesh;
-            }
+            if (shouldDisplayLowerSide) UpdateMesh(ref _lowerObj, interLow, lowerMesh, lowerEbo);
 
-            if (shouldDisplayUpperSide)
-            {
-                _upperObj = Instantiate(srcObject);
-                _upperObj.GetComponent<MeshFilter>().sharedMesh = interUp.CreateMesh();
-
-                upperMesh.triangles = upperEbo.ToArray();
-                srcObject.GetComponent<MeshFilter>().sharedMesh = upperMesh;
-            }
+            if (shouldDisplayUpperSide) UpdateMesh(ref _upperObj, interUp, upperMesh, upperEbo);
         }
 
-        private void CreateTriangle(Intersector inter, int i, Vector3 objVert1, Vector3 objVert2, Vector3 objVert3,  bool isFirstLower, bool isSecondLower, bool isThirdLower)
+        private void UpdateMesh(ref GameObject borderObj, Intersector intersector, Mesh mesh, List<int> triangles)
+        {
+            borderObj = Object.Instantiate(_srcObject, _srcObject.transform.parent);
+            foreach (Transform child in borderObj.transform) {
+                MonoBehaviour.Destroy(child.gameObject);
+            }
+            
+            borderObj.GetComponent<MeshFilter>().sharedMesh = intersector.CreateMesh();
+
+            mesh.triangles = triangles.ToArray();
+            _srcObject.GetComponent<MeshFilter>().sharedMesh = mesh;
+
+            borderObj.transform.position = _srcObject.transform.position;
+            borderObj.transform.rotation = _srcObject.transform.rotation;
+            borderObj.transform.localScale = _srcObject.transform.localScale;
+        }
+
+        private void CreateTriangle(Intersector inter, int i, Vector3 objVert1, Vector3 objVert2, Vector3 objVert3,
+            bool isFirstLower, bool isSecondLower, bool isThirdLower)
         {
             if (isFirstLower && !isSecondLower && !isThirdLower)
-            {
                 inter.CreateTriangleOnePointOnLeft(objVert1, objVert2, objVert3, i, 0, 1, 2);
-            }
             else if (isSecondLower && !isFirstLower && !isThirdLower)
-            {
                 inter.CreateTriangleOnePointOnLeft(objVert2, objVert1, objVert3, i, 1, 0, 2);
-            }
             else if (isThirdLower && !isFirstLower && !isSecondLower)
-            {
                 inter.CreateTriangleOnePointOnLeft(objVert3, objVert1, objVert2, i, 2, 0, 1);
-            }
             else if (isFirstLower && isSecondLower && !isThirdLower)
-            {
                 inter.CreateTrianglesTwoPointsOnLeft(objVert1, objVert2, objVert3, i, 0, 1, 2);
-            }
             else if (!isFirstLower && isSecondLower && isThirdLower)
-            {
                 inter.CreateTrianglesTwoPointsOnLeft(objVert2, objVert3, objVert1, i, 1, 2, 0);
-            }
             else if (isFirstLower && !isSecondLower && isThirdLower)
-            {
                 inter.CreateTrianglesTwoPointsOnLeft(objVert1, objVert3, objVert2, i, 0, 2, 1);
-            }
-        }
-        private void Start()
-        {
-            _mesh = srcObject.GetComponent<MeshFilter>().sharedMesh;
-            _slicerMesh = slicerQuad.GetComponent<MeshFilter>().sharedMesh;
-            Slice();
-            _prevSlicerPos = slicerQuad.transform.position;
-            _prevSlicerRotation = slicerQuad.transform.rotation.eulerAngles;
-        }
-
-        private void Update()
-        {
-            if ((slicerQuad.transform.position - _prevSlicerPos).magnitude > 0.001f ||
-                (slicerQuad.transform.rotation.eulerAngles - _prevSlicerRotation).magnitude > 0.001f)
-            {
-                Destroy(_lowerObj);
-                Destroy(_upperObj);
-                Slice();
-            }
-
-            _prevSlicerPos = slicerQuad.transform.position;
-            _prevSlicerRotation = slicerQuad.transform.eulerAngles;
         }
     }
 }
